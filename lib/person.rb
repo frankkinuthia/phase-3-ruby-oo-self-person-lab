ac5b3cd720edd43e1ea821dc73a9da0393e3c0ce
Frank <f.kinuthia01@gmail.com>
2023-03-22 09:00:09 +0300

    refactored code

--- lib/person.rb
+++ lib/person.rb
@@ -1,81 +1,70 @@
 # your code goes here
 
 class Person
-
-  attr_reader :name
-  attr_accessor :bank_account, :happiness, :hygiene
-
-    def initialize (name, bank_account=25, happiness=8, hygiene=8)
-
-        @name=name
-        @bank_account=bank_account
-        @happiness=happiness
-        @hygiene=hygiene
-
-    end
-
-    def hygiene=(value)
-        @hygiene=[value, 0,10].sort[1]
-
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
     end
     
     def happiness=(value)
-        @happiness=[value, 0,10].sort[1]
-
+      @happiness = value.clamp(0, 10)
     end
-
-    def happy?
-        self.happiness>7
-
+    
+    def hygiene=(value)
+      @hygiene = value.clamp(0, 10)
     end
-
+    
     def clean?
-        self.hygiene>7
-
+      hygiene > 7
     end
-
+    
+    def happy?
+      happiness > 7
+    end
+    
     def get_paid(amount)
-        self.bank_account +=amount
-        "all about the benjamins" 
-
+      self.bank_account += amount
+      "all about the benjamins"
     end
-
+    
     def take_bath
-        self.hygiene +=4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
+      self.hygiene += 4
+      "♪ Rub-a-dub just relaxing in the tub ♫"
     end
-
+    
     def work_out
-        self.hygiene -=3
-        self.happiness +=2
-        "♪ another one bites the dust ♫"
+      self.happiness += 2
+      self.hygiene -= 3
+      "♪ another one bites the dust ♫"
     end
-
-    def call_friend(person)
-        self.happiness +=3
-        person.happiness +=3
-        "Hi #{person.name}! It's #{self.name}. How are you?"
-
+    
+    def call_friend(friend)
+      self.happiness += 3
+      friend.happiness += 3
+      "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
-
-    def start_conversation(person, topic)
-        if topic=="politics"
-            person.happiness -=2
-            self.happiness-=2
-            'blah blah partisan blah lobbyist'
-        elsif topic=="weather"
-            person.happiness +=1
-            self.happiness +=1
-            'blah blah sun blah rain'
-
-        else
-            "blah blah blah blah blah"
-
+    
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+            when "weather"
+                self.happiness += 1
+                friend.happiness += 1
+                "blah blah sun blah rain"
+            else
+                "blah blah blah blah blah"
         end
-
     end
-
-end 
+end
+  
 
 # person1 = Person.new("Alice")
 # person2 = Person.new("Bob")
