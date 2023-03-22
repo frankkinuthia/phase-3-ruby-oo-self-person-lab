0374eb924bec173a045cad7775a80aed4218c3a1
Frank <f.kinuthia01@gmail.com>
2023-03-22 08:56:06 +0300

    Tested and commented out test code

--- lib/person.rb
+++ lib/person.rb
@@ -14,15 +14,16 @@ class Person
 
     end
 
+    def hygiene=(value)
+        @hygiene=[value, 0,10].sort[1]
+
+    end
+    
     def happiness=(value)
         @happiness=[value, 0,10].sort[1]
 
     end
 
-    def hygiene=(value)
-        @hygiene=[value, 0,10].sort[1]
-
-    end
     def happy?
         self.happiness>7
 
@@ -75,3 +76,54 @@ class Person
     end
 
 end 
+
+# person1 = Person.new("Alice")
+# person2 = Person.new("Bob")
+
+# puts person1.name #"Alice"
+# puts person1.bank_account # 25
+# person1.bank_account = 100
+# puts person1.bank_account #100
+
+
+# puts person1.happiness #8
+# person1.happiness += 5
+# puts person1.happiness #10
+# person1.happiness += 3
+# puts person1.happiness #10
+
+# puts person1.hygiene #8
+# person1.hygiene -= 10
+# puts person1.hygiene #0
+# person1.hygiene -= 3
+# puts person1.hygiene #0
+
+# puts person1.clean? #false
+# person1.hygiene += 4
+# puts person1.clean?  #true
+
+# puts person1.happy? # false
+# person1.happiness += 4
+# puts person1.happy? # true
+
+# puts person1.get_paid(500) # "all about the benjamins"
+# puts person1.bank_account # 600
+
+# puts person1.take_bath # "♪ Rub-a-dub just relaxing in the tub ♫"
+# puts person1.hygiene # 10
+
+# puts person1.work_out # "♪ another one bites the dust ♫"
+# puts person1.happiness # 10
+# puts person1.hygiene # 7
+
+# puts person1.call_friend(person2) # "Hi Bob! It's Alice. How are you?"
+# puts person1.happiness # 10
+# puts person2.happiness # 11
+
+# puts person1.start_conversation(person2, "politics") # "blah blah partisan blah lobbyist"
+# puts person1.happiness # 8
+# puts person2.happiness # 9
+
+# puts person1.start_conversation(person2, "weather") # "blah blah sun blah
+
+
